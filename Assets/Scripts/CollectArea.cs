bda267eedb1c0e07f83338e3808ae6b0dd4d2417
Ensar Balık <ensarbalk@gmail.com>
2023-01-11 12:49:23 +0300

    CollectArea

--- Assets/Scripts/CollectArea.cs
+++ Assets/Scripts/CollectArea.cs
@@ -7,14 +7,32 @@ using UnityEngine;
 public class CollectArea : MonoBehaviour
 {
     public Stack stack;
-
     public int total;
-    
+
+    public GameObject money;
+
+    public float addInterval;
+
     private void OnTriggerStay(Collider other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
             stack.StackMoney(total);
+        }
+    }
+
+    private void Update()
+    {
+        
+        StackMoney();
+    }
+
+    public void StackMoney(int addCount = 1)
+    {
+        for (int i = 0; i < addCount; i++)
+        {
+            if (total <= 0) return;
+            Transform spawnedObject = Instantiate(money).transform;
         }
     }
 }
