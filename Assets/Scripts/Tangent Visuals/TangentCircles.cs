15a0e53bbc7c59a1c35b9e2443a177948bd35247
Ensar Balık <ensarbalk@gmail.com>
2023-01-17 13:18:20 +0300

    Final

--- Assets/Scripts/Tangent Visuals/TangentCircles.cs
+++ Assets/Scripts/Tangent Visuals/TangentCircles.cs
@@ -15,10 +15,10 @@ public class TangentCircles : CircleTangent
     {
         _innerCircleGo = Instantiate(circlePrefab);
         _outterCircleGo = Instantiate(circlePrefab);
-        
+
         _tangentCircle = new Vector4[circleAmount];
         _tangentObjects = new GameObject[circleAmount];
-        
+
         for (int i = 0; i < circleAmount; i++)
         {
             GameObject tangentInstance = Instantiate(circlePrefab);
@@ -31,7 +31,7 @@ public class TangentCircles : CircleTangent
     {
         _innerCircleGo.transform.position = new Vector3(innerCircle.x, innerCircle.y, innerCircle.z);
         _innerCircleGo.transform.localScale = new Vector3(innerCircle.w, innerCircle.w, innerCircle.w) * 2;
-        
+
         _outterCircleGo.transform.position = new Vector3(outerCircle.x, outerCircle.y, outerCircle.z);
         _outterCircleGo.transform.localScale = new Vector3(outerCircle.w, outerCircle.w, outerCircle.w) * 2;
 
@@ -41,7 +41,7 @@ public class TangentCircles : CircleTangent
             _tangentObjects[i].transform.position =
                 new Vector3(_tangentCircle[i].x, _tangentCircle[i].y, _tangentCircle[i].z);
             _tangentObjects[i].transform.localScale =
-                new Vector3(_tangentCircle[i].w, _tangentCircle[i].w, _tangentCircle[i].w);
+                new Vector3(_tangentCircle[i].w, _tangentCircle[i].w, _tangentCircle[i].w) * 2;
         }
     }
 }
