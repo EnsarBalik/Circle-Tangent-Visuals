ac1a5e85b5d14ab06e6cbca06a8d5346f2d5242c
Ensar Balık <ensarbalk@gmail.com>
2023-01-17 11:42:50 +0300

    Circle Tangent Visuals

---
+++ Assets/Scripts/Tangent Visuals/TangentCircles.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TangentCircles : CircleTangent
+{
+    public GameObject circlePrefab;
+    private GameObject _innerCircleGo, _outterCircleGo;
+    public Vector4 innerCircle, outerCircle;
+
+    void Start()
+    {
+        _innerCircleGo = Instantiate(circlePrefab);
+        _outterCircleGo = Instantiate(circlePrefab);
+    }
+
+    void Update()
+    {
+        _innerCircleGo.transform.position = new Vector3(innerCircle.x, innerCircle.y, innerCircle.z);
+        _innerCircleGo.transform.position = new Vector3(innerCircle.w, innerCircle.w, innerCircle.w) * 2;
+        
+        _outterCircleGo.transform.position = new Vector3(outerCircle.x, outerCircle.y, outerCircle.z);
+        _outterCircleGo.transform.position = new Vector3(outerCircle.w, outerCircle.w, outerCircle.w) * 2;
+    }
+}
