1d9787c815f4ed62dca28361b04f626f68fb0f35
Ensar Balık <ensarbalk@gmail.com>
2023-01-17 13:06:55 +0300

    Tangent

--- Assets/Scripts/Tangent Visuals/TangentCircles.cs
+++ Assets/Scripts/Tangent Visuals/TangentCircles.cs
@@ -5,16 +5,26 @@ using UnityEngine;
 public class TangentCircles : CircleTangent
 {
     public GameObject circlePrefab;
-    private GameObject _innerCircleGo, _outterCircleGo,_tangentCircleGo;
+    private GameObject _innerCircleGo, _outterCircleGo;
     public Vector4 innerCircle, outerCircle;
-    public float tangentCircleRadius;
-    public float degree;
+    private Vector4[] _tangentCircle;
+    private GameObject[] _tangentObjects;
+    [Range(1, 64)] public int circleAmount;
 
     void Start()
     {
         _innerCircleGo = Instantiate(circlePrefab);
         _outterCircleGo = Instantiate(circlePrefab);
-        _tangentCircleGo = Instantiate(circlePrefab);
+        
+        _tangentCircle = new Vector4[circleAmount];
+        _tangentObjects = new GameObject[circleAmount];
+        
+        for (int i = 0; i < circleAmount; i++)
+        {
+            GameObject tangentInstance = Instantiate(circlePrefab);
+            _tangentObjects[i] = tangentInstance;
+            _tangentObjects[i].transform.parent = transform;
+        }
     }
 
     void Update()
@@ -25,9 +35,13 @@ public class TangentCircles : CircleTangent
         _outterCircleGo.transform.position = new Vector3(outerCircle.x, outerCircle.y, outerCircle.z);
         _outterCircleGo.transform.localScale = new Vector3(outerCircle.w, outerCircle.w, outerCircle.w) * 2;
 
-        _tangentCircleGo.transform.position =
-            GetRotatedTangent(degree, outerCircle.w) + _outterCircleGo.transform.position;
-        _tangentCircleGo.transform.localScale =
-            new Vector3(tangentCircleRadius, tangentCircleRadius, tangentCircleRadius) * 2;
+        for (int i = 0; i < circleAmount; i++)
+        {
+            _tangentCircle[i] = FindTangentCircle(outerCircle, innerCircle, (360f / circleAmount) * i);
+            _tangentObjects[i].transform.position =
+                new Vector3(_tangentCircle[i].x, _tangentCircle[i].y, _tangentCircle[i].z);
+            _tangentObjects[i].transform.localScale =
+                new Vector3(_tangentCircle[i].w, _tangentCircle[i].w, _tangentCircle[i].w);
+        }
     }
 }
