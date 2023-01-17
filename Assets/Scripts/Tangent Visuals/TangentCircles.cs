2a44e781a5c049bd4c59f7211d9a391cc56f506d
Ensar Balık <ensarbalk@gmail.com>
2023-01-17 12:03:15 +0300

    Update TangentCircles.cs

--- Assets/Scripts/Tangent Visuals/TangentCircles.cs
+++ Assets/Scripts/Tangent Visuals/TangentCircles.cs
@@ -17,9 +17,9 @@ public class TangentCircles : CircleTangent
     void Update()
     {
         _innerCircleGo.transform.position = new Vector3(innerCircle.x, innerCircle.y, innerCircle.z);
-        _innerCircleGo.transform.position = new Vector3(innerCircle.w, innerCircle.w, innerCircle.w) * 2;
+        _innerCircleGo.transform.localScale = new Vector3(innerCircle.w, innerCircle.w, innerCircle.w) * 2;
         
         _outterCircleGo.transform.position = new Vector3(outerCircle.x, outerCircle.y, outerCircle.z);
-        _outterCircleGo.transform.position = new Vector3(outerCircle.w, outerCircle.w, outerCircle.w) * 2;
+        _outterCircleGo.transform.localScale = new Vector3(outerCircle.w, outerCircle.w, outerCircle.w) * 2;
     }
 }
